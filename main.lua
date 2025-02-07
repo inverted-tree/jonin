b58bfafbbfc3081c940804ab28b1d23e372aa3e4
Lukas Schröder <lukasscr@proton.me>
2025-02-07 21:15:25 +0100

    Basic directory I/O testing
    Testing out some basic file system I/O and printing the content of the
    current working directory.

--- main.lua
+++ main.lua
@@ -2,6 +2,22 @@
 
 local function main()
 	print("Hello world from the jougetsu build system!")
+
+	local handle, err = io.popen("ls")
+	if not handle then
+		print("ERROR: Failed to execute 'ls': " .. tostring(err))
+		os.exit(1)
+	end
+
+	local result = handle:read("*a")
+	handle:close()
+
+	if result then
+		print("The current directory content is:")
+		print(result)
+	else
+		print("ERROR: No output recieved from 'ls'.")
+	end
 end
 
 main()

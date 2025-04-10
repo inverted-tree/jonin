becc85c8bcadc40e4fa4da5ed2f17df031378c8c
Lukas Schröder <lukasscr@proton.me>
2025-04-10 20:05:19 +0200

    Improve reference build script
    Added comments to the reference build script that is linked in the
    README.md file for clearer instructions.

--- test/build-options.lua
+++ test/build-options.lua
@@ -1,12 +1,19 @@
+-- This build script serves as a reference to write your own build-options.lua script.
+
+-- Requiring this module is always necessary
 require("jonin")
 
-Target("HelloWorld", {
-	cc = "clang",
-	cflags = {
-		"-Wall",
-		"-Wextra",
-	},
-	files = {
-		"./test/hello-world/src/",
-	},
-})
+-- This function defines a new build target:
+Target(
+	"HelloWorld", -- name of the target
+	{
+		cc = "clang", -- set the compiler (optional, defaults to 'cc')
+		cflags = { -- set compile flags to be used by your compiler
+			"-Wall",
+			"-Wextra",
+		},
+		files = { -- define the source files to use in this build (globbing with '*' is supported)
+			"./test/hello-world/src/",
+		},
+	}
+)

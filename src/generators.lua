348cd5831fe07ca5d93cd5d1749925e9e4375724
Lukas Schröder <lukasscr@proton.me>
2025-07-06 16:58:32 +0200

    Remove lua code
    The C++ code now does (most of) the stuff the lua implementation did, so
    the lua code can be removed.

--- src/generators.lua
+++
@@ -1,8 +0,0 @@
--- The generators module offers a straight-forward interface to define the components that make up the Ninja build file
-
-return {
-	binding = require("src.components.binding"),
-	rule = require("src.components.rule"),
-	statement = require("src.components.statement"),
-	target = require("src.components.target"),
-}
